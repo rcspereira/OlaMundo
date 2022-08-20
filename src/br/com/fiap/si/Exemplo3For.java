df647fb02c90745ab851a74040428d5f3be0362e
rcspe <rcspereira@hotmail.com>
2022-08-20 19:19:35 -0300

    inclusão da classes novas

--- src/br/com/fiap/si/Exemplo3For.java
+++ src/br/com/fiap/si/Exemplo3For.java
@@ -3,7 +3,7 @@ package br.com.fiap.si;
 public class Exemplo3For {
     public static void main(String[] args) {
         int cont = 1;
-        while (cont <= 12) {
+        while (cont <= 12) { //substituído 'for' por 'while' por sugestão da IDE
             System.out.println(cont);
             cont++;
         }
